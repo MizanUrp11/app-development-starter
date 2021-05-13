412f2e44d17b9be7e3d7d51e3995cbd0a2d3ea44
MizanUrp11 <mrn1105009@gmail.com>
2021-05-13 15:18:02 +0600

    Password module

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+app.get("/",(req,res)=>{
+    res.end("Hello world2");
+})
 
 const _port = process.env.PORT || 4000;
 app.listen(_port,e=>console.log(`Server Started at port ${_port}`));

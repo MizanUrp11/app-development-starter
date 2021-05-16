5aa8aefeb963e592e528df784f46f8f96d0e017b
MizanUrp11 <mrn1105009@gmail.com>
2021-05-16 16:34:13 +0600

    Resource_List_and_Transform

--- src/controllers/redirects.js
+++ src/controllers/redirects.js
@@ -1,29 +1,38 @@
 const router = require("express").Router();
 const { Direction } = require("../utils/db");
-const {check} = require("express-validator");
+const { check } = require("express-validator");
 const _p = require("../utils/promis_errors");
 const reject_invalid = require("../middlewares/reject_invalid");
 
 const entryValidator = [check('url').isURL()];
-router.post("/api/v1/redirects",entryValidator,reject_invalid,async (req,res,next)=>{
+router.post("/api/v1/redirects", entryValidator, reject_invalid, async (req, res, next) => {
     let user_id = req.user.id;
     let destination = req.body.url;
-    let timestamp = Date.now()/1000;
-    console.log("user_id:"+ user_id);
-    console.log("timestamp:"+ timestamp);
-
+    let timestamp = Date.now() / 1000;
     let hash = parseInt(`${user_id}${timestamp}`).toString(32);
-    let [cretErr,created] = await _p(Direction.create({
-        user_id,destination,hash
+    let [cretErr, created] = await _p(Direction.create({
+        user_id, destination, hash
     }));
-    if(cretErr && !created){
+
+    if (cretErr && !created) {
         next(cretErr);
-    }else{
+    } else {
         res.json({
-            message:"Direction created successfully",
+            message: "Direction created successfully",
             hash
         })
     }
+})
+
+router.get("/api/v1/redirects", async (req, res) => {
+    let [dberr, myDirections] = await _p(Direction.findAll({
+        where: {
+            'user_id': req.user.id
+        },
+        limit: 10
+    }));
+    if (dberr) return next(dberr);
+    return res.json(myDirections.map(d => { return { id: d.id, destination: d.destination, hash: d.hash, createdAt: d.createdAt } }))
 })
 
 module.exports = router;

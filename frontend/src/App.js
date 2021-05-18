48678d08e629e0a22653bc5a15bb98f621ea0129
MizanUrp11 <mrn1105009@gmail.com>
2021-05-18 23:12:09 +0600

    Adding React Router for navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import Loginsignup from "./components/loginsignup";
+import Urls from "./components/urls";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Loginsignup/>
+      <Router>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/urls" component={Urls}></Route>
+        <Route path="/loginsignup" component={Loginsignup}></Route>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <Link to="/loginsignup">login sign up</Link>
+      <Link to="/urls">Urls</Link>
     </div>
   );
 }
---
+++ frontend/src/components/urls.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Urls extends Component {
+  
+  render() {
+    return (
+      <div>
+          <h2>Hello urls</h2>
+      </div>
+    );
+  }
+}
+
+export default Urls;
